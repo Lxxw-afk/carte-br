15f4245a35e9c914a351a7b5b5c6550000e8730a
Lxxw-afk <medou76260@gmail.com>
2025-12-02 15:59:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const mapImg = document.getElementById("map");
+
+let zoom = 1;
+let posX = 0;
+let posY = 0;
+let isDragging = false;
+let startX, startY;
+
+mapImg.style.transformOrigin = "0 0";
+
+document.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    zoom += e.deltaY * -0.001;
+    zoom = Math.min(Math.max(zoom, 0.5), 3);
+    updateTransform();
+});
+
+mapImg.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.clientX - posX;
+    startY = e.clientY - posY;
+});
+
+document.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    posX = e.clientX - startX;
+    posY = e.clientY - startY;
+    updateTransform();
+});
+
+document.addEventL
